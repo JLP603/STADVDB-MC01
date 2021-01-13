43e518a728722a47219527d2a7328dfc630db851
JLP603 <60498189+JLP603@users.noreply.github.com>
2021-01-13 21:11:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ db.connect(function (err) {
 
 
 const app = express();
-const port = 3000;
+const port = 9000;
 //originally const port= 9000;
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
