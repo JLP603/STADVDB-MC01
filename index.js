c32c53154ec9b49fe57b8c5211bd3a2222093220
JLP603 <60498189+JLP603@users.noreply.github.com>
2021-01-13 21:22:21 +0800

    deployment test

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ db.connect(function (err) {
 
 
 const app = express();
-const port = 9000;
+const port = process.event.PORT || 9000;
 //originally const port= 9000;
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
